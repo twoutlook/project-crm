19343e63117ad80a0ba0efc59120073e02dda0df
Mark <twcloudwebapp@outlook.com>
2016-05-28 18:37:23 +0800

    try echo  ->

--- roundcubemail/test1.php
+++ roundcubemail/test1.php
@@ -42,7 +42,11 @@ require_once 'program/include/iniset.php';
 // init application, start session, init output class, etc.
 $RCMAIL = rcmail::get_instance(0, $GLOBALS['env']);
 
-print_r($RCMAIL->action);
+//print_r($RCMAIL->action);
+echo '$RCMAIL->action<br>';
+
+echo $RCMAIL->action;
+
 echo "<hr>";
 
 print_r($RCMAIL);
